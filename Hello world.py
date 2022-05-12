ba98720c369ccf2fb0c83128f0b5462e9743a4fe
Hebersonmiliano <70893385+Hebersonmiliano@users.noreply.github.com>
2022-05-12 20:02:28 -0300

    Add files via upload

---
+++ Hello world.py
@@ -0,0 +1,6 @@
+Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/efg/AppData/Local/Programs/Python/Python38/teste.py ====
+Hello world
+>>> 
